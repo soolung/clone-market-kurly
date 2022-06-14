e9f5953598765c9924c35ec334b74e98b3f8ca5f
gimhanul <gimhanuly@gmail.com>
2022-06-14 12:20:09 +0900

    ADD :: 야매 로그인 (#11)
    - 야매 로그인 기능 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,10 +6,51 @@ import {useState} from "react";
 export default function Login() {
 
     const [securityCheck, setSecurityCheck] = useState(true);
+    const [loginInput, setLoginInput] = useState({
+        id: "",
+        password: "",
+    });
 
     const toggleSecurityCheck = () => {
         setSecurityCheck(!securityCheck);
-    }
+    };
+
+    const changeLoginInput = e => {
+        setLoginInput({
+            ...loginInput,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const login = () => {
+        if (loginInput.id === "gimhanul" && loginInput.password === "gimhanul") {
+            const user = {
+                name: "김한울",
+                numberOfCart: 3,
+                grade: "일반",
+                delivery: {
+                    address: "경기도 평택시 상서재로5길 15 (평택센트럴자이1단지) 104동 101호",
+                    type: "택배배송",
+                },
+            }
+
+            sessionStorage.setItem("currentUser", JSON.stringify(user));
+        } else if (loginInput.id === "sookyoungwoo" && loginInput.password === "sookyoungwoo") {
+            const user = {
+                name: "우수경",
+                numberOfCart: 0,
+                grade: "더퍼플",
+                delivery: {
+                    address: "동래버스에서 내려서 갈 수 있는 온천천 근처 오두막집에서 왼쪼긍로 꺾으렴",
+                    type: "샛별배송",
+                },
+            }
+
+            sessionStorage.setItem("currentUser", JSON.stringify(user));
+        } else {
+            alert("아이디 또는 비밀번호 오류입니다.")
+        }
+    };
 
     return (
         <section>
@@ -18,8 +59,8 @@ export default function Login() {
                     <h3 className="title">로그인</h3>
 
                     <div className="login-box">
-                        <input type="text" className="id" placeholder="아이디를 입력해주세요"/>
-                        <input type="password" className="password" placeholder="비밀번호를 입력해주세요"/>
+                        <input type="text" name="id" className="id" placeholder="아이디를 입력해주세요" onChange={changeLoginInput}/>
+                        <input type="password" name="password" className="password" placeholder="비밀번호를 입력해주세요" onChange={changeLoginInput}/>
                         <div className="check-box">
                             <div className="security-check">
                                 <Check id="security" willDo={toggleSecurityCheck} isChecked={securityCheck}/>
@@ -41,6 +82,7 @@ export default function Login() {
                                 color="purple"
                                 text="로그인"
                                 className="login"
+                                willDo={login}
                             />
                         </a>
                         <a href="">
