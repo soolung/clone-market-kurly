52dfdb4c7c6fa4b5ea2463e5ac50692d1a4538df
gimhanul <gimhanuly@gmail.com>
2022-06-14 10:27:22 +0900

    ADD :: box-sizing
    - padding, margin 값이 변경되어도 width 값이 바뀌지 않도록 하는 설정 추가
    - 그에 따른 header 값 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+        </>
+    );
 }
 
 export default App;
