c7b245983fdb444101e070d119400b0ace4c48a3
gimhanul <gimhanuly@gmail.com>
2022-06-18 23:16:28 +0900

    ADD :: Main Page (#13)
    - main page 추가
    - 기타 짜ㅏㅈㄹ한 일

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,27 @@
 import BannerData from "./banner.json";
+import ProductData from "./productArticleData.json"
 import Banner from "../../components/Main/Banner/Banner"
-import ProductSection from "../../components/Main/ProductSection/ProductSection";
+import ProductArticle from "../../components/Main/ProductArticle/ProductArticle";
 
 export default function Main() {
+    let i = 0
 
     return (
-        <>
+        <section>
             <Banner
                 banner={BannerData.banners}
             />
-        </>
+            {
+                ProductData.sections.map(s => (
+                    <ProductArticle
+                        title={s.title}
+                        description={s.description}
+                        hasListPage={s.hasListPage}
+                        num={i++}
+                        products={s.products}
+                    />
+                ))
+            }
+        </section>
     )
 }
