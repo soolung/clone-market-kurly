b6a5c24aa25f24e2cc6b9af991f16bf1713024b7
gimhanul <gimhanuly@gmail.com>
2022-06-22 22:39:23 +0900

    ADD :: AmmountButton className 추가 (#21)

--- src/components/AmountButton/AmountButton.js
+++ src/components/AmountButton/AmountButton.js
@@ -2,7 +2,7 @@ import "./AmountButton.scss";
 
 export default function AmountButton(props) {
     return (
-        <div className="amount-count">
+        <div className={`amount-count ${props.className}`}>
             <button onClick={props.minus} className="amount-count-button amount-count-minus on" />
             <input className="amount-count-value" value={props.amount} readOnly={true}/>
             <button onClick={props.plus} className="amount-count-button amount-count-plus on" />
